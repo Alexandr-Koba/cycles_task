dddea8b0b9c6ff90a9d85f681c2ee8b894f1b8e3
AlexandrKoba <babakoba@yandex.ru>
2023-01-15 18:50:00 +0700

    'Commit Class Def FightClub'

---
+++ project/Fight_Club2.py
@@ -0,0 +1,24 @@
+class Hero:
+    def __init__(self, name, age, damage, armor, health):
+        self.name = name
+        self.age = age
+        self.damage = damage
+        self.armor = armor
+        self.health = health
+    
+    def person(self):
+        print(f"Имя {self.name}, Возраст {self.age}")
+
+    def fight(self):
+        print(f"Сила - {self.damage}\nБроня - {self.armor}\nЗдоровье - {self.health}")
+
+    def info(self):
+        print(f"\nИмя - {self.name}\nВозраст - {self.age}\nСила - {self.damage}\nБроня - {self.armor}\nЗдоровье - {self.health}")
+        
+
+dima = Hero('Демон', 78, 80, 30, 220)
+frank = Hero('Громила', 20, 95, 10, 330)
+
+
+dima.info()
+frank.info()
